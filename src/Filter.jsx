9191973f73759dee61eb2759315ff358bc3c5c44
Shannon2023Fall <148981251+Shannon2023Fall@users.noreply.github.com>
2023-12-04 21:12:20 +0100

    Update Filter.jsx

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,29 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Accidents from './EScooterAccidents.json';
 
 
 export default function Filter() {
-    {/* Why this const[] needs to be inside of function not over it? */ }
+    
     const [crash, setCrash] = useState(Accidents);
     console.log(crash);
+    const [sortedData, setSortedData] = useState([]);
 
-    const filterCrash =(catCrash)=>{
-        const updateCrash = Accidents.filter((curCrash)=>{
-            return curCrash.gender === catCrash
+    const filterCrash =(driverCrash)=>{
+        const updateCrash = Accidents.filter((genderCrash)=>{
+            return genderCrash.gender === driverCrash
         });
         setCrash(updateCrash);
     }
 
+    //const monthNames = {'January':1, 'February':2,'March':3,'April':4,'Mai':5,'June':6,'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}
+    //useEffect(() => {
+        //Data = () => setCrash(Accidents) ? Accidents : super(driverCrash);
+        //switch(sortBy, action){
+        //    case 'Sort by Age':{return Data.sort((a,b) => a.age > b.age ? 1 : -1) }
+        //    case 'Sort by Month':{return Data.sort((a,b) => monthNames[a.month] > monthNames[b.month] ? 1 : -1)}
+        //}
+    //    const sorted = filterCrash.sort((a,b)=> a.age > b.age ? 1 : -1);
+    //    setSortedData(sorted);
+    //});
+
     return (
         <div className="container mt-3 mb-5">
             <h2>Bochum EScooter Accidents reported on news from year 2022</h2>
-            <button type="button" class="btn btn-info me-3 mb-2" onClick={()=>setCrash(Accidents)}>All</button>
-            <button type="button" class="btn btn-info me-3 mb-2" onClick={()=>filterCrash("male")}>Male Driver</button>
-            <button type="button" class="btn btn-info me-3 mb-2"onClick={()=>filterCrash("female")}>Female Driver</button>
+            <button type="button" class="btn btn-primary me-3 mb-2" onClick={()=>setCrash(Accidents)}>All Reported</button>
+            <button type="button" class="btn btn-secondary me-3 mb-2" onClick={()=>filterCrash("male")}>Male Driver</button>
+            <button type="button" class="btn btn-secondary me-3 mb-2" onClick={()=>filterCrash("female")}>Female Driver</button>
+            {/*<button type="button" class="btn btn-success me-3 mb-2" onClick={filterCrash.sort((a,b)=>a.age > b.age ? 1 : -1)}>Sort by Age</button>*/}
+            {/*<button type="button" class="btn btn-success me-3 mb-2">Sort by Month</button>*/}
 
             <div className="row">
                 {/* A map is the standard JavaScript function and also a type of data collection. It offers a fast searching of data. */}
-                {crash.map((acci) => (
+                {crash
+                .sort((a,b) => a.age > b.age ? 1 : -1)
+                .map((acci) => (
                     <div className = "col-md-auto" >
                     <div class="card" >
                         <div class="card-header">
